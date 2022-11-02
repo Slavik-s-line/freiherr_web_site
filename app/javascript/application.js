692921b7d859e841a64a6eac58114414ce668f31
Viacheslav Sukhenko <vsukhenko.codica@gmail.com>
2022-11-02 23:14:48 +0200

    add main page

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,6 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+import "@fortawesome/fontawesome-free"
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
