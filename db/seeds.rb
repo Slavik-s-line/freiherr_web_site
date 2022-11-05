8315a9c282f5372ae4341e4bebf9e2c9d58157c3
Viacheslav Sukhenko <slavichmy@gmail.com>
2022-11-05 19:00:31 +0200

    edit seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
 Music.create(title: 'Night at Work | Instrumental Chill Music Mix',
              description: 'An instrumental chill mix featuring the finest in deep, calm and atmospheric electronic music, with genres such as chillout, downtempo and future garage. Uses for the music include music for background, working, studying, concentration, focusing, stress relief, etc.',
              url: 'https://www.youtube.com/embed/n9Y2Eb4BaSg')
